eb52d34e53b9998e1ab31689d8d3779eade7bce5
Daemon <92645380+DaemonWAGMI@users.noreply.github.com>
2022-03-18 16:10:58 -0400

    Update: Adjust puppeteer to run on heroku

--- src/functions/offers.js
+++ src/functions/offers.js
@@ -69,7 +69,10 @@ const offersByUrl = async (url, optionsGiven = {}) => {
   if (!customPuppeteerProvided) {
     browser = await puppeteer.launch({
       headless: !debug, // when debug is true => headless should be false
-      args: ['--start-maximized'],
+      args: [
+        '--start-maximized',
+        '--no-sandbox',
+      ],
     });
   }
   customPuppeteerProvided && warnIfNotUsingStealth(browser);
